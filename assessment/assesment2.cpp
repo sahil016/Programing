0110388967850b6fe52af5197e478a3c238f93fd
Sahil <90812888+sahil016@users.noreply.github.com>
2024-11-17 12:40:54 +0530

    assement2

---
+++ assessment/assesment2.cpp
@@ -0,0 +1,13 @@
+#inlcude<iostream>
+using namesapce std:
+	char name;
+	cout>>"-----------------------------Tops Tech. Fast Food--------------------------------";
+	cout>>"Please enter your name: ";
+	sin<<name;
+	menu()
+	{
+		
+	}
+	
+	
+main()
