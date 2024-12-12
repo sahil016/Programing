a5c4aa9eb1dcaf9749c91f7153c28668ab3a77b7
Sahil <90812888+sahil016@users.noreply.github.com>
2024-12-13 04:20:11 +0530

    numberM

---
+++ lab work/test_py/number_manipulation.py
@@ -0,0 +1,60 @@
+""" fac=1
+n=int(input("Enter number : "))
+for i in range (1,n+1):
+    fac*=i
+print("factorian is : ",fac) 
+
+"""
+
+
+""" 
+for i in range(1,5):
+    print(" "*(5-i),"*"*i)
+    
+print("************************************")    
+for i in range (1,6):
+    print(" "*(6-i),"*"*i) 
+    
+"""
+
+# t=('sahil',1,2,3,4,4)
+
+# t2=list(t)
+
+# t2.append(100)
+# t2.pop(0)
+
+# t3=tuple(t2)
+
+# # print(t2.count(4))
+
+# print("t3 elements: ",*t3) 
+
+""" l = [1,2,1,1.45,6.24,"hello","python",True]
+
+l.append(100) # adds new element in the last
+print(l)
+
+print(l.count(1))
+l.extend([100,600,800]) # addelemetn
+print(l)
+l.insert(2,"antima") #insert the value on sertain index
+print(l)
+
+l.pop(0)#remuvee the endex value
+print(l)
+l.remove(1.45) #remove the value
+l.reverse()#revers the list
+print(l)  """
+
+
+""" 
+l=[1,2,3,4,5555,53,21,76,453,745,32,54,111]
+for i in range(len(l)-1,-1,-1):
+    print(l[i],end=" ")
+"""
+
+# n=int(input("num: "))
+# for i in range(1,n+1):
+#     print(i)  
+
