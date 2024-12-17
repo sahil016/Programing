0604214b400842db0c6ee17899061617be56eb61
Sahil <90812888+sahil016@users.noreply.github.com>
2024-12-17 16:15:29 +0530

    hi2

---
+++ assignment/Module-14/PracticalEx/Dictionaries.py
@@ -0,0 +1,5 @@
+# 13) Write a Python program to create a dictionary of 6 key-value pairs.
+
+
+
+# 14) Write a Python program to access values using keys from a dictionary
--- assignment/Module-14/PracticalEx/list_Op.py
+++ assignment/Module-14/PracticalEx/list_Op.py
@@ -13,8 +13,23 @@ print("After insert(2, 3):", my_list)
 
 
 # 4) Write a Python program to remove elements from a list using pop() and remove().
-""" list=["sahil","Diwan",2004]
+""" list=["sahil","Hello",2004]
 list.pop(0)
-print(list) #['Diwan']
-list.remove("Diwan")
-print(list) """
+print(list) #['Hello']
+list.remove("Hello")
+print(list)  """
+
+
+#5) Write a Python program to iterate through a list and print each element. 
+""" l=["Hello","world",12334,True]
+for element in l:
+    print(element)
+ """
+
+# 6) Write a Python program to insert elements into an empty list using a for loop and append().
+""" l=[]
+for i in range(1,5):
+    l.append(i)
+print(l)
+ """
+
