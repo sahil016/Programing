2ed1a62e9beeb73050495badd00ab26dca402625
sahil016 <sahildiwan8888@gmail.com>
2025-03-25 12:05:30 +0530

    asd

--- assessment(python)/assement2py/db.py
+++ assessment(python)/assement2py/db.py
@@ -10,7 +10,7 @@ mydb.commit()
 mydb = pymysql.connect(host="localhost",user="root",password="",database="hotel")
 mycursor = mydb.cursor()
 
-mycursor.execute("create table if not exists customer(id int primary key auto_increment,name varchar(20),contact bigint,email varchar(25), gender varchar(10),city varchar (15),state varchar (15) )")
+mycursor.execute("create table if not exists user(id int primary key auto_increment,name varchar(20),contact bigint,email varchar(25), gender varchar(10),city varchar (15),state varchar (15) )")
 mydb.commit()
 
 
